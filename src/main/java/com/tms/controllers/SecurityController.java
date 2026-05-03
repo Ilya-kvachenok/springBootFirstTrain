b61c6ca64b81d8145327180b8125e441072a4649
kva4 <kvachenokilya2006@gmail.com>
2026-05-03 06:00:55 +0300

    FreeMarker

--- src/main/java/com/tms/controllers/SecurityController.java
+++ src/main/java/com/tms/controllers/SecurityController.java
@@ -1,20 +1,15 @@
 package com.tms.controllers;
 
-import com.tms.exceptions.AgeException;
-import com.tms.model.User;
 import com.tms.model.dto.RegistrationRequestDto;
 import com.tms.model.dto.UserDto;
 import com.tms.services.SecurityService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/security") // Перед всеми путями методов этого контроллера будет стоять /security
